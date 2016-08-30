3304fe33feed3e758f0a4c82b7955199ef53408b
Kun Liu <david@blitzm.com>
2016-08-30 13:55:07 +1000

    Initial Commit

---
+++ ChatTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatTemplate
+//
+//  Created by Kun Liu on 30/08/2016.
+//  Copyright © 2016 Blitzm System. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatTemplate
+//
+//  Created by Kun Liu on 30/08/2016.
+//  Copyright © 2016 Blitzm System. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
